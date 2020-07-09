9ca02251d7172128dceb29fed40bec0b824fb86c
Jitesh <pawanjitesh93@gmail.com>
2020-07-09 17:38:23 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormGroupDirective, NgForm } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { User } from '../user';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+export interface DialogData {
+  username: string;
+}
+
+interface Course {
+  value: string;
+  viewValue: string;
+}
+
+@Component({
+  selector: 'dialog-overview-example-dialog',
+  templateUrl: '../dialog-overview-example-dialog.html'
+})
+export class DialogOverviewExampleDialog {
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {}
+
+  onClickOk(): void {
+    this.dialogRef.close();
+  }
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +35,122 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'registrationLogin';
+
+  constructor(public dialog: MatDialog) {}
+
+  registrationForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    userName: new FormControl(''),
+    course: new FormControl(''),
+    gender: new FormControl(''),
+    address: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+    email: new FormControl('', [
+      Validators.email
+    ]),
+    birthDate: new FormControl(''),
+    age: new FormControl(''),
+    phone: new FormControl(''),
+  });
+
+  loginForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl('')
+  });
+  defaultValue;
+
+  courses: Course[] = [
+    {value: 'geology-0', viewValue: 'Geology'},
+    {value: 'physiology-1', viewValue: 'Physiology'},
+    {value: 'pshycology-2', viewValue: 'Pshycology'}
+  ];
+
+  registered: boolean = false;
+  login: boolean = true;
+  logged: boolean = true;
+  logError: boolean = true;
+  passwordMatch: boolean = false;
+  usernameMatcher: boolean = false;
+
+  users: User[] = [];
+  usernames: string[] = [];
+
+  onRegister() {
+    if(this.registrationForm.controls['password'].value == this.registrationForm.controls['confirmPassword'].value) {
+      this.registered = true;
+      this.passwordMatch = false;
+      let user = new User(this.registrationForm.get('userName').value, this.registrationForm.get('password').value);
+      if(this.usernames.indexOf(this.registrationForm.controls['userName'].value) != -1) {
+        this.usernameMatcher = true;
+      } else {
+        this.usernameMatcher = false;
+        this.usernames.push(this.registrationForm.get('userName').value);
+        this.users.push(user);
+      }
+    } else {
+      this.registered = false;
+      this.passwordMatch = true;
+    }
+
+    if(this.usernameMatcher) {
+      this.registered = false;
+      const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+        width: '250px',
+        data: {username: this.registrationForm.controls['userName'].value}
+      });
+
+      dialogRef.afterClosed().subscribe(result => {
+        console.log('The dialog was closed');
+        this.registrationForm.controls['userName'] = result;
+      });
+      this.usernameMatcher = false;
+    }
+    console.log(this.users);
+  }
+
+  onLogin() {
+    this.registered = true;
+    let usernameIndex = this.users.findIndex(x => x.username == this.loginForm.controls['username'].value);
+    let passwordIndex = this.users.findIndex(x => x.password == this.loginForm.controls['password'].value);
+    if(usernameIndex >= 0 && passwordIndex >= 0 && usernameIndex == passwordIndex) {
+      this.logError = true;
+      this.logged = false;
+    } else {
+      this.logError = false;
+      this.logged = true;
+    }
+    console.log(this.loginForm.value);
+  }
+
+  calculateAge(event) {
+    let today: Date = new Date;
+    let dob = event.value;
+    let timeDiff = Math.abs(Date.now() - dob.getTime());
+    let age = Math.floor((timeDiff / (1000 * 3600 * 24)) / 365.25);
+    this.registrationForm.controls['age'].setValue(age);
+    console.log(age);
+  }
+
+  ngOnInit() {
+    this.registrationForm.controls.proof.patchValue(this.defaultValue);
+  }
+
+  emailMatcher = new EmailErrorStateMatcher();
+  passwordMatcher = new PasswordErrorStateMatcher();
+}
+
+export class EmailErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+
+export class PasswordErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl): boolean {
+    if(control.parent.value['password'] != control.value) return true;
+    return false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
@@ -10,9 +19,21 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatDialogModule
   ],
-  providers: [],
+  providers: [
+    MatDatepickerModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
